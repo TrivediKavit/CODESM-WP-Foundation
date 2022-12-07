b47dca195815a99eda3199aabed80acb78c61d70
Kavit Trivedi <knt1993@gmail.com>
2022-12-07 19:07:34 +0530

    - Update 404.php

--- 404.php
+++ 404.php
@@ -19,11 +19,11 @@ get_header(); ?>
 				<article class="content-not-found">
 				
 					<header class="article-header">
-						<h1><?php _e( 'Epic 404 - Article Not Found', 'jointswp' ); ?></h1>
+						<h1><?php _e( 'Epic 404 - Page Not Found', 'jointswp' ); ?></h1>
 					</header> <!-- end article header -->
 			
 					<section class="entry-content">
-						<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'jointswp' ); ?></p>
+						<p><?php _e( 'The page you were looking for was not found, but maybe try looking again!', 'jointswp' ); ?></p>
 					</section> <!-- end article section -->
 					
 					<?php /*
